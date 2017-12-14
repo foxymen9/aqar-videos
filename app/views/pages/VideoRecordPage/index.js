14a36b12ea64e63c44d9feb785d4676711797785
tosmos <foxymen9@gmail.com>
2017-12-14 23:13:35 +0300

    add video page

--- app/views/pages/VideoRecordPage/index.js
+++ app/views/pages/VideoRecordPage/index.js
@@ -11,18 +11,90 @@ import {
 import { styles } from './styles';
 import Container from '../../layout/Container';
 
+import Camera from 'react-native-camera';
+
 export default class VideoRecordPage extends Component {
-  startRecord() {
-    
+  constructor(props) {
+    super(props);
+    this.camera = null;
+
+    this.state = {
+      flashMode: 'auto',
+      type: 'back',
+      captureTarget: Camera.constants.CaptureTarget.cameraRoll,
+      aspect: Camera.constants.Aspect.fill,
+      orientation: Camera.constants.Orientation.auto,
+      isRecording: false,
+      isStart: false,
+    }
   }
+  
+  startRecording = () => {
+    this.setState({isStart: true});
+    console.log('START');
+
+    if (this.camera) {
+      this.camera.capture({mode: Camera.constants.CaptureMode.video})
+          .then((data) => {
+            console.log('captured_data', data)
+          })
+          .catch(err => console.error(err));
+
+      this.setState({
+        isRecording: true
+      });
+    }
+  }
+
+  stopRecording = () => {
+    this.setState({isStart: false});
+    console.log('STOP');
+
+    if (this.camera) {
+      this.camera.stopCapture();
+      this.setState({
+        isRecording: false
+      });
+    }
+  }
+
   render() {
+    const {isStart} = this.state;
     return (
       <Container title='VIDEO'>
         <View style={styles.container}>
-          <TouchableOpacity onPress={()=>this.startRecord()}>
-            <Text>Start</Text>
-          </TouchableOpacity>
-          
+          <View style={styles.timeView}>
+            <Text style={styles.textTime}></Text>
+          </View>
+          <View style={styles.btnView}>
+            {!isStart
+            ?  <TouchableOpacity onPress={()=>this.startRecording()} style={styles.btnRecordView}>
+                <View style={styles.btnRecord}>
+                  <View style={styles.btnRecordInnerStart} />
+                </View>
+              </TouchableOpacity>
+            :  <TouchableOpacity onPress={()=>this.stopRecording()} style={styles.btnRecordView}>
+                <View style={styles.btnRecord}>
+                  <View style={styles.btnRecordInnerStop} />
+                </View>
+              </TouchableOpacity>
+            }
+          </View>
+          <Camera
+            ref={(cam) => {
+              this.camera = cam;
+            }}
+            style={styles.preview}
+            aspect={this.state.aspect}
+            captureTarget={this.state.captureTarget}
+            type={this.state.type}
+            flashMode={this.state.flashMode}
+            onFocusChanged={() => {}}
+            onZoomChanged={() => {}}
+            defaultTouchToFocus
+            mirrorImage={false}
+          >
+          </Camera>
         </View>
       </Container>
     )
--- app/views/pages/VideoRecordPage/styles.js
+++ app/views/pages/VideoRecordPage/styles.js
@@ -11,5 +11,57 @@ export const styles = StyleSheet.create({
     width: '100%',
     justifyContent: 'flex-start',
     alignItems: 'center',
+    backgroundColor: '#EFEFEF',
   },
+  preview: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  btnView: {
+    position: 'absolute',
+    bottom: 40,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    zIndex: 99,
+  },
+  btnRecordView: {
+    marginHorizontal: 5,
+  },
+  btnRecord: {
+    width: 60,
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+    borderWidth: 5,
+    borderColor: 'white',
+    borderRadius: 30,
+  },
+  btnRecordInnerStart: {
+    width: 40,
+    height: 40,
+    backgroundColor: commonColors.pinkColor,
+    borderRadius: 20,
+  },
+  btnRecordInnerStop: {
+    width: 30,
+    height: 30,
+    backgroundColor: commonColors.pinkColor,
+    borderRadius: 5,
+  },
+  timeView: {
+    position: 'absolute',
+    top: 20,
+    backgroundColor: 'transparent',
+    alignItems: 'center',
+    justifyContent: 'center',
+    zIndex: 99,
+  },
+  textTime: {
+    color: 'white',
+    fontSize: 14,
+    backgroundColor: 'transparent'
+  }
 });
