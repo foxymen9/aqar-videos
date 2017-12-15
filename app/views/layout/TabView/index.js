46cf549ced4d193f0d0a8125f4823ca24e816754
tosmos <foxymen9@gmail.com>
2017-12-15 21:31:41 +0300

    add current location in map

--- app/views/layout/TabView/index.js
+++ app/views/layout/TabView/index.js
@@ -17,17 +17,53 @@ import ScrollableTabBar from './ScrollableTabBar';
 
 import { connect } from 'react-redux';
 import { Actions } from 'react-native-router-flux';
+
 import { styles } from './styles';
+import * as commonStyles from '../../../common/styles/commonStyles';
 
 import MapPage from '../../pages/MapPage';
 import ProductListPage from '../../pages/ProductListPage';
 
+const ASPECT_RATIO = commonStyles.screenWidth / commonStyles.screenHeight
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
 class TabView extends Component {
   constructor(props) {
     super(props);
+    watchID:  null;
+
     this.state = {
       tabIndex: 0,
+      region: null,
+      currentLocation: null,
     }
+  }
+
+  componentDidMount() {
+    const {region} = this.state;
+    
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      let region = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      }
+      let currentLocation = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      }
+
+      this.setState({
+        region: region,
+        currentLocation: currentLocation
+      })
+    })
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
   }
 
   changeTab(index) {
@@ -38,8 +74,84 @@ class TabView extends Component {
 
   render() {
     const {btnStatus} = this.props;
-    const {tabIndex} = this.state;
+    const { tabIndex, region, currentLocation } = this.state;
     
+    if (currentLocation == null || region == null) {
+      return null;
+    }
+    
+    let buildingData = [
+      {
+        coordinates: {
+          latitude: currentLocation.latitude - 0.02,
+          longitude: currentLocation.longitude
+        },
+        data: {
+          price: '100K',
+          image: 'https://ar.rdcpix.com/1310744609/3d220b868bac74f582f666970f984894c-f0xd-w1020_h770_q80.jpg',
+          title: 'Test Title1'
+        }
+      },
+      {
+        coordinates: {
+          latitude: currentLocation.latitude + 0.01,
+          longitude: currentLocation.longitude - 0.01
+        },
+        data: {
+          price: '90K',
+          image: 'https://ar.rdcpix.com/1310744609/3d220b868bac74f582f666970f984894c-f0xd-w1020_h770_q80.jpg',
+          title: 'Test Title2'
+        }
+      },
+      {
+        coordinates: {
+          latitude: currentLocation.latitude - 0.01,
+          longitude: currentLocation.longitude + 0.01
+        },
+        data: {
+          price: '80K',
+          image: 'https://ar.rdcpix.com/1310744609/3d220b868bac74f582f666970f984894c-f0xd-w1020_h770_q80.jpg',
+          title: 'Test Title3'
+        }
+      }
+    ];
+
+    let apartmentData = [
+      {
+        coordinates: {
+          latitude: currentLocation.latitude - 0.02,
+          longitude: currentLocation.longitude - 0.01
+        },
+        data: {
+          price: '100K',
+          image: 'https://ar.rdcpix.com/1310744609/3d220b868bac74f582f666970f984894c-f0xd-w1020_h770_q80.jpg',
+          title: 'Test Title1'
+        }
+      },
+      {
+        coordinates: {
+          latitude: currentLocation.latitude + 0.01,
+          longitude: currentLocation.longitude + 0.01
+        },
+        data: {
+          price: '90K',
+          image: 'https://ar.rdcpix.com/1310744609/3d220b868bac74f582f666970f984894c-f0xd-w1020_h770_q80.jpg',
+          title: 'Test Title2'
+        }
+      },
+      {
+        coordinates: {
+          latitude: currentLocation.latitude + 0.02,
+          longitude: currentLocation.longitude - 0.01
+        },
+        data: {
+          price: '80K',
+          image: 'https://ar.rdcpix.com/1310744609/3d220b868bac74f582f666970f984894c-f0xd-w1020_h770_q80.jpg',
+          title: 'Test Title3'
+        }
+      }
+    ];
+
     return (
       <View style={styles.container}>
         <ScrollableTabView
@@ -53,7 +165,7 @@ class TabView extends Component {
         >
           <ScrollView tabLabel="Building">
             {btnStatus == 'map' && (
-              <MapPage page="building" />
+              <MapPage page="building" locationData={buildingData} region={region} />
             )}
             {btnStatus == 'list' && (
               <ProductListPage />
@@ -61,7 +173,7 @@ class TabView extends Component {
           </ScrollView>
           <ScrollView tabLabel="Apartment">
             {btnStatus == 'map' && (
-              <MapPage page="apartment" />
+              <MapPage page="apartment" locationData={apartmentData} region={region} />
             )}
             {btnStatus == 'list' && (
               <ProductListPage />
--- app/views/pages/MapPage/index.js
+++ app/views/pages/MapPage/index.js
@@ -17,34 +17,9 @@ import * as commonStyles from '../../../common/styles/commonStyles';
 
 const icon_bubble = require('../../../common/assets/images/map/speech_bubble.png');
 
-const ASPECT_RATIO = commonStyles.screenWidth / commonStyles.screenHeight
-const LATITUDE_DELTA = 0.0922;
-const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
-const LATITUDE = 37.798790;
-const LONGITUDE = -122.442753;
-
 export default class MapPage extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      region: {
-        latitude: LATITUDE,
-        longitude: LONGITUDE,
-        latitudeDelta: LATITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA,
-      },
-      currentLocation: {
-        latitude: LATITUDE,
-        longitude: LONGITUDE
-      },
-    }
-  }
-
-  componentDidMount() {
-  }
-
-  componentWillUnmount() {
-    navigator.geolocation.clearWatch(this.watchID);
   }
 
   gotoDetailPage(data) {
@@ -52,51 +27,14 @@ export default class MapPage extends Component {
   }
 
   render() {
-    const { region, currentLocation, isBtnList } = this.state;
-    // if (coordinates == null || currentLocation == null || region == null)
-    //   return null;
-
-    let locationData = [
-      {
-        coordinates: {
-          latitude: 37.788780,
-          longitude: -122.442753
-        },
-        data: {
-          price: '100K',
-          image: 'https://ar.rdcpix.com/1310744609/3d220b868bac74f582f666970f984894c-f0xd-w1020_h770_q80.jpg',
-          title: 'Test Title1'
-        }
-      },
-      {
-        coordinates: {
-          latitude: 37.798781,
-          longitude: -122.422753
-        },
-        data: {
-          price: '90K',
-          image: 'https://ar.rdcpix.com/1310744609/3d220b868bac74f582f666970f984894c-f0xd-w1020_h770_q80.jpg',
-          title: 'Test Title2'
-        }
-      },
-      {
-        coordinates: {
-          latitude: 37.778780,
-          longitude: -122.452724
-        },
-        data: {
-          price: '80K',
-          image: 'https://ar.rdcpix.com/1310744609/3d220b868bac74f582f666970f984894c-f0xd-w1020_h770_q80.jpg',
-          title: 'Test Title3'
-        }
-      }
-    ];
+    const { locationData, region } = this.props;
 
     return (
       <View style={styles.container}>
         <MapView
           style={ styles.mapView }
           initialRegion={ region }
+          showsUserLocation={true}
         >
           {locationData.map((marker, index) => (
             <MapView.Marker
