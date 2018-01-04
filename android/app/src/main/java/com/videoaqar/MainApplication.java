ce3c9316c0716e2f915f619b774e63a338b9cbe2
tosmos <foxymen9@gmail.com>
2018-01-04 22:44:51 +0300

    change app name before app crashed

--- android/app/src/main/java/com/videoaqar/MainApplication.java
+++ android/app/src/main/java/com/videoaqar/MainApplication.java
@@ -1,4 +1,4 @@
-package com.aqarvideos;
+package com.videoaqar;
 
 import android.app.Application;
 
--- ios/VideoAqarTests/VideoAqarTests.m
+++ ios/VideoAqarTests/VideoAqarTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface AqarVideosTests : XCTestCase
+@interface VideoAqarTests : XCTestCase
 
 @end
 
-@implementation AqarVideosTests
+@implementation VideoAqarTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
